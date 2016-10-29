b0f6a73835fd2cba585ccc011c7655407182268e
Tetiana Pidhurska <tetiana_pidhurska@epam.com>
2016-10-29 22:53:42 +0300

    JpaRunner works

--- src/main/java/ua/rd/pizzaservice/runner/JPAAppRunner.java
+++ src/main/java/ua/rd/pizzaservice/runner/JPAAppRunner.java
@@ -14,36 +14,36 @@ import java.math.BigDecimal;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
-@Transactional
+
 public class JPAAppRunner {
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpa");
         EntityManager em = emf.createEntityManager();
 
-//        Pizza p1 = new Pizza("Sea Pizza", new BigDecimal(1), Pizza.PizzaType.SEA);
+        Pizza p1 = new Pizza("Sea Pizza", new BigDecimal(1), Pizza.PizzaType.SEA);
 //        Pizza p2 = new Pizza("New York Style Pizza", new BigDecimal(1), Pizza.PizzaType.MEAT);
 //        Pizza p3 = new Pizza("New Pizza", new BigDecimal(1), Pizza.PizzaType.VEGETERIAN);
         PizzaCard card = createPizzaCard();
 
 
         Map<Pizza, Integer> pizzas1 = new HashMap<>();
-        pizzas1.put(new Pizza("Bavarian", new BigDecimal(1), Pizza.PizzaType.MEAT), 1);
+        pizzas1.put(p1, 1);
 
 
 
         Customer customer = createCustomer();
         customer.setCard(card);
-        Order order = new Order(pizzas1, new Customer("Ivan"));
+        Order order = new Order(pizzas1, customer);
 
 
         EntityTransaction entityTransaction = em.getTransaction();
         entityTransaction.begin();
 
-//        em.persist(p1);
+        em.persist(p1);
 //        em.persist(p2);
 //        em.persist(p3);
-//        em.persist(card);
-//        em.persist(customer);
+//       em.persist(card);
+//       em.persist(customer);
         em.persist(order);
 
         entityTransaction.commit();
